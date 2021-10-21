09445d1c0e24e31ce8efca62d3e465adf53e4435
atlx <41349879+atlx@users.noreply.github.com>
2021-10-20 23:00:03 -0400

    Create 'namespace.rs', implement most of 'pass_manager.rs'

--- src/pass.rs
+++ src/pass.rs
@@ -1,4 +1,4 @@
-use crate::{block, diagnostic, function, int_kind, node, prototype, void_kind};
+use crate::{block, diagnostic, function, int_kind, node, pass_manager, prototype, void_kind};
 
 pub struct PassContext {
   diagnostics: Vec<diagnostic::Diagnostic>,
@@ -8,8 +8,12 @@ pub type PassResult = Result<(), diagnostic::Diagnostic>;
 
 pub trait Pass {
   // TODO:
-  fn register(&self) -> bool {
+  fn register(&self, pass_manager: &pass_manager::PassManager) -> bool {
     return true;
+  }
+
+  fn get_diagnostics(&self) -> Vec<diagnostic::Diagnostic> {
+    vec![]
   }
 
   fn visit(&mut self, node: &dyn node::Node) -> PassResult {
